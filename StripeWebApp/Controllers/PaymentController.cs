4f49cb4ebb6d97ec13279822c4f841269233dc04
Ghulam Mahiu Din <109981291+Ghulam-MahiuDin@users.noreply.github.com>
2024-05-29 12:07:33 +0500

    Add project files.

---
+++ StripeWebApp/Controllers/PaymentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.BillingPortal;
+using Stripe.Checkout;
+
+namespace StripeWebApp.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create()
+        {
+            var domain = "http://localhost:4242";
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    Price = "price_1PLfksHaaqG3CBCGar0G1PdA",
+                    Quantity = 1,
+                  },
+                },
+                Mode = "payment",
+                SuccessUrl = domain + "/Payment/Success.cshtml",
+                CancelUrl = domain + "/Payment/Cancel.cshtml",
+            };
+            var service = new Stripe.Checkout.SessionService();
+            Stripe.Checkout.Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+    }
+}
